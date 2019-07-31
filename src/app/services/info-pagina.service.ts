f87537b3050e6bcc3c4db2f09ce366a711aa3be9
Maria Moreno <mariale.contreras@gmail.com>
2019-07-31 13:20:27 +0200

    Carga de la información de Firebase a nuestra página

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { url } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,31 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
-        // Leer el archivo json
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
+    // Leer el archivo json
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
+      // console.log(resp);
       // console.log( resp['email'] );
     });
   }
+
+  private cargarEquipo(){
+    //Leer el archivo json
+    this.http.get('https://angular-html-24b0d.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
 }
