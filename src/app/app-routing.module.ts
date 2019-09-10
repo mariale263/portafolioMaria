e2bf1fb40a15f99590a0540bc0691aecb112c477
Maria Moreno <mariale.contreras@gmail.com>
2019-09-10 14:37:58 +0200

    Creando la pagina de busqueda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { Routes, RouterModule} from '@angular/router';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+import { SearchComponent } from './pages/search/search.component';
 
+// tslint:disable-next-line:variable-name
 const app_routes: Routes = [
     { path: 'home', component: PortafolioComponent},
     { path: 'about', component: AboutComponent},
     { path: 'item/:id', component: ItemComponent},
+    { path: 'search/:termino', component: SearchComponent},
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
 
 ];
@@ -16,7 +19,7 @@ const app_routes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot( app_routes, { useHash: true})
-    ], 
+    ],
     exports: [
         RouterModule
     ]
